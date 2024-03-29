d15da2cfc8ce7ef474cfc6947d2657654c59707d
derslik1-11 <sinifsoftito@gmail.com>
2024-03-29 14:29:36 +0300

    Add - User Repository Interface. Add - User Repository.

---
+++ WatchWise/Repositories/IUserRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity;
+using WatchWise.Models;
+
+namespace WatchWise.Repositories
+{
+	public interface IUserRepository
+	{
+        IQueryable<WatchWiseUser> GetAllUsers();
+        WatchWiseUser? GetUserById(long id);
+        IdentityResult AddUser(WatchWiseUser watchWiseUser, string password);
+        void UpdateUser(WatchWiseUser watchWiseUser);
+    }
+}
+
---
+++ WatchWise/Repositories/UserRepository.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity;
+using WatchWise.Models;
+using WatchWise.Services;
+
+namespace WatchWise.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly SignInManager<WatchWiseUser> _signInManager;
+
+        public UserRepository(SignInManager<WatchWiseUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
+        public IQueryable<WatchWiseUser> GetAllUsers()
+        {
+            return _signInManager.UserManager.Users;
+        }
+
+        public WatchWiseUser? GetUserById(long id)
+        {
+            return _signInManager.UserManager.Users.FirstOrDefault(u => u.Id == id);
+        }
+
+        public IdentityResult AddUser(WatchWiseUser watchWiseUser, string password)
+        {
+            return _signInManager.UserManager.CreateAsync(watchWiseUser, password).Result;
+        } 
+
+        public void UpdateUser(WatchWiseUser watchWiseUser)
+        {
+            _signInManager.UserManager.UpdateAsync(watchWiseUser).Wait();
+        }
+    }
+}
+
