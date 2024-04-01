dfb2a1bb07eed87377da57e9338a3a59c859c445
derslik1-11 <sinifsoftito@gmail.com>
2024-04-01 12:07:45 +0300

    Add - Response DTO’s for Director, Person Add - Converter for Director

---
+++ WatchWise/DTOs/Converters/DirectorConverter.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.ComponentModel;
+using WatchWise.DTOs.Requests;
+using WatchWise.DTOs.Responses;
+using WatchWise.Models;
+
+namespace WatchWise.DTOs.Converters
+{
+	public class DirectorConverter
+	{
+        public Director Convert(DirectorRequest directorRequest)
+		{
+			Director newDirector = new()
+			{
+				Name = directorRequest.Name,
+				MediaDirectors = new List<MediaDirector>()
+			};
+			return newDirector;
+		}
+
+        public DirectorResponse Convert(Director director)
+        {
+            DirectorResponse directorResponse = new()
+            {
+				Id = director.Id,
+                Name = director.Name,
+                MediaDirectors = new List<MediaDirector>()
+            };
+			if (director.MediaDirectors != null)
+			{
+                directorResponse.MediaDirectors = director.MediaDirectors;
+
+            }
+            return directorResponse;
+        }
+
+        public List<DirectorResponse> Convert(List<Director> directors)
+        {
+            List<DirectorResponse> directorResponses = new();
+            foreach (Director director in directors)
+            {
+                directorResponses.Add(Convert(director));
+            }
+            return directorResponses;
+        }
+    }
+}
+
---
+++ WatchWise/DTOs/Responses/DirectorResponse.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using WatchWise.Models;
+
+namespace WatchWise.DTOs.Responses
+{
+	public class DirectorResponse : PersonResponse
+	{
+        public List<MediaDirector>? MediaDirectors { get; set; }
+    }
+}
+
