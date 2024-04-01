59980015fe6290051a41c5b03637a1e2ea55eb17
derslik1-11 <sinifsoftito@gmail.com>
2024-04-01 12:25:57 +0300

    Add - Star Response Add - Star Converter

---
+++ WatchWise/DTOs/Converters/StarConverter.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using WatchWise.DTOs.Requests;
+using WatchWise.DTOs.Responses;
+using WatchWise.Models;
+
+namespace WatchWise.DTOs.Converters
+{
+	public class StarConverter
+	{
+        public Star Convert(StarRequest starRequest)
+        {
+            Star newStar = new()
+            {
+                Name = starRequest.Name,
+                MediaStars = new List<MediaStar>()
+            };
+            return newStar;
+        }
+
+        public StarResponse Convert(Star star)
+        {
+            StarResponse starResponse = new()
+            {
+                Id = star.Id,
+                Name = star.Name,
+                MediaStars = new List<MediaStar>()
+            };
+            if (star.MediaStars != null)
+            {
+                starResponse.MediaStars = star.MediaStars;
+
+            }
+            return starResponse;
+        }
+
+        public List<StarResponse> Convert(List<Star> stars)
+        {
+            List<StarResponse> starResponses = new();
+            foreach (Star star in stars)
+            {
+                starResponses.Add(Convert(star));
+            }
+            return starResponses;
+        }
+    }
+}
+
---
+++ WatchWise/DTOs/Responses/StarResponse.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using WatchWise.Models;
+
+namespace WatchWise.DTOs.Responses
+{
+	public class StarResponse : PersonResponse
+	{
+        public List<MediaStar>? MediaStars { get; set; }
+    }
+}
+
