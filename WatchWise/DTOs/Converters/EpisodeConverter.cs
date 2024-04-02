e6399734d75e92bab3d3c9ff0bd8ba29d0a6f224
derslik1-11 <sinifsoftito@gmail.com>
2024-04-02 13:25:32 +0300

    Add - Episode DTO’s and Converter

---
+++ WatchWise/DTOs/Converters/EpisodeConverter.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using WatchWise.DTOs.Requests;
+using WatchWise.DTOs.Responses;
+using WatchWise.Models;
+using WatchWise.Models.CrossTables;
+
+namespace WatchWise.DTOs.Converters
+{
+	public class EpisodeConverter
+	{
+        public Episode Convert(EpisodeRequest episodeRequest)
+        {
+            Episode newEpisode = new()
+            {
+                MediaId = episodeRequest.MediaId,
+                SeasonNum = episodeRequest.SeasonNum,
+                EpisodeNum = episodeRequest.EpisodeNum,
+                ReleaseDate = episodeRequest.ReleaseDate,
+                Title = episodeRequest.Title,
+                Description = episodeRequest.Description,
+                Duration = episodeRequest.Duration,
+                ViewCount = 0,
+                Passive = false,
+                UserWatchedEpisodes = new List<UserWatchedEpisode>()
+            };
+            return newEpisode;
+        }
+
+        public EpisodeResponse Convert(Episode episode)
+        {
+            EpisodeResponse episodeResponse = new()
+            {
+                Id = episode.Id,
+                MediaId = episode.MediaId,
+                SeasonNum = episode.SeasonNum,
+                EpisodeNum = episode.EpisodeNum,
+                ReleaseDate = episode.ReleaseDate,
+                Title = episode.Title,
+                Description = episode.Description,
+                Duration = episode.Duration,
+                ViewCount = episode.ViewCount,
+                Passive = episode.Passive,
+                UserWatchedEpisodes = new List<UserWatchedEpisode>()
+            };
+            if (episode.UserWatchedEpisodes != null)
+            {
+                episodeResponse.UserWatchedEpisodes = episode.UserWatchedEpisodes;
+            }
+            return episodeResponse;
+        }
+
+        public List<EpisodeResponse> Convert(List<Episode> episodes)
+        {
+            List<EpisodeResponse> episodeResponses = new();
+            foreach (Episode episode in episodes)
+            {
+                episodeResponses.Add(Convert(episode));
+            }
+            return episodeResponses;
+        }
+    }
+}
+
---
+++ WatchWise/DTOs/Requests/EpisodeRequest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using WatchWise.Models;
+using WatchWise.Models.CrossTables;
+
+namespace WatchWise.DTOs.Requests
+{
+	public class EpisodeRequest
+	{
+        public int MediaId { get; set; }
+
+        [Range(0, byte.MaxValue)]
+        public byte SeasonNum { get; set; }
+
+        [Range(0, 366)]
+        public short EpisodeNum { get; set; }
+
+        public DateTime ReleaseDate { get; set; }
+
+        public string Title { get; set; } = null!;
+
+        [StringLength(500)]
+        public string? Description { get; set; }
+
+        public TimeSpan Duration { get; set; }
+    }
+}
+
