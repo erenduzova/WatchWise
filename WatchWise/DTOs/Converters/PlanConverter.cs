3aff2632471a5c5f37d57eee0b410cf4551e61f3
derslik1-11 <sinifsoftito@gmail.com>
2024-04-02 09:58:31 +0300

    Add - Plan Request Add - Plan Response Add - Plan Converter

---
+++ WatchWise/DTOs/Converters/PlanConverter.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using WatchWise.DTOs.Requests;
+using WatchWise.DTOs.Responses;
+using WatchWise.Models;
+using WatchWise.Models.CrossTables;
+
+namespace WatchWise.DTOs.Converters
+{
+	public class PlanConverter
+	{
+        public Plan Convert(PlanRequest planRequest)
+        {
+            Plan newPlan = new()
+            {
+                Name = planRequest.Name,
+                Price = planRequest.Price,
+                MaxResolution = planRequest.MaxResolution,
+                UserPlans = new List<UserPlan>()
+            };
+            return newPlan;
+        }
+
+        public PlanResponse Convert(Plan plan)
+        {
+            PlanResponse planResponse = new()
+            {
+                Id = plan.Id,
+                Name = plan.Name,
+                Price = plan.Price,
+                MaxResolution = plan.MaxResolution,
+                UserPlans = new List<UserPlan>()
+            };
+            if (plan.UserPlans != null)
+            {
+                planResponse.UserPlans = plan.UserPlans;
+            }
+            return planResponse;
+        }
+
+        public List<PlanResponse> Convert(List<Plan> plans)
+        {
+            List<PlanResponse> planResponse = new();
+            foreach (Plan plan in plans)
+            {
+                planResponse.Add(Convert(plan));
+            }
+            return planResponse;
+        }
+    }
+}
+
---
+++ WatchWise/DTOs/Requests/PlanRequest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using WatchWise.Models.CrossTables;
+
+namespace WatchWise.DTOs.Requests
+{
+	public class PlanRequest
+	{
+        [StringLength(50, MinimumLength = 2)]
+        public string Name { get; set; } = null!;
+
+        [Range(0, float.MaxValue)]
+        public float Price { get; set; }
+
+        [StringLength(20, MinimumLength = 2)]
+        public string MaxResolution { get; set; } = null!;
+    }
+}
+
