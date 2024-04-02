7e25419cf096cde48a8691d2be4ade449006bd69
derslik1-11 <sinifsoftito@gmail.com>
2024-04-02 15:48:00 +0300

    Add - Episode Service and Interface

---
+++ WatchWise/Services/Implementations/EpisodeService.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using WatchWise.DTOs.Converters;
+using WatchWise.DTOs.Requests;
+using WatchWise.DTOs.Responses;
+using WatchWise.Models;
+using WatchWise.Repositories.Implementations;
+using WatchWise.Repositories.Interfaces;
+using WatchWise.Services.Interfaces;
+
+namespace WatchWise.Services.Implementations
+{
+    public class EpisodeService : IEpisodeService
+    {
+        private readonly IEpisodeRepository _episodeRepository;
+        private readonly EpisodeConverter _episodeConverter;
+
+        public EpisodeService(IEpisodeRepository episodeRepository, EpisodeConverter episodeConverter)
+        {
+            _episodeRepository = episodeRepository;
+            _episodeConverter = episodeConverter;
+        }
+
+        public List<EpisodeResponse> GetAllEpisodeResponses()
+        {
+            IQueryable<Episode> episodes = _episodeRepository.GetAllEpisodes(includeUserWatchedEpisodes: true);
+            return _episodeConverter.Convert(episodes.AsNoTracking().ToList());
+        }
+
+        public EpisodeResponse? GetEpisodeResponseById(long id)
+        {
+            Episode? foundEpisode = _episodeRepository.GetEpisodeById(id, includeUserWatchedEpisodes: true);
+            if (foundEpisode != null)
+            {
+                return _episodeConverter.Convert(foundEpisode);
+            }
+            return null;
+        }
+
+        public void PostEpisode(EpisodeRequest episodeRequest)
+        {
+            Episode newEpisode = _episodeConverter.Convert(episodeRequest);
+            _episodeRepository.AddEpisode(newEpisode);
+        }
+
+        public int DeleteEpisode(long id)
+        {
+            Episode? foundEpisode = _episodeRepository.GetEpisodeById(id);
+            if (foundEpisode != null)
+            {
+                foundEpisode.Passive = true;
+                _episodeRepository.UpdateEpisode(foundEpisode);
+                return 1;
+            }
+            return -1;
+        }
+
+        public int UpdateEpisode(long id, EpisodeUpdateRequest episodeUpdateRequest)
+        {
+            Episode? foundEpisode = _episodeRepository.GetEpisodeById(id);
+            if (foundEpisode != null)
+            {
+                if (episodeUpdateRequest.Title != null)
+                {
+                    foundEpisode.Title = episodeUpdateRequest.Title;
+                }
+                if (episodeUpdateRequest.Description != null)
+                {
+                    foundEpisode.Description = episodeUpdateRequest.Description;
+                }
+                _episodeRepository.UpdateEpisode(foundEpisode);
+                return 1;
+            }
+            return -1;
+        }
+    }
+}
+
---
+++ WatchWise/Services/Interfaces/IEpisodeService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using WatchWise.DTOs.Requests;
+using WatchWise.DTOs.Responses;
+
+namespace WatchWise.Services.Interfaces
+{
+	public interface IEpisodeService
+	{
+        List<EpisodeResponse> GetAllEpisodeResponses();
+        EpisodeResponse? GetEpisodeResponseById(long id);
+        void PostEpisode(EpisodeRequest episodeRequest);
+        int UpdateEpisode(long id, EpisodeUpdateRequest episodeUpdateRequest);
+        int DeleteEpisode(long id);
+    }
+}
+
