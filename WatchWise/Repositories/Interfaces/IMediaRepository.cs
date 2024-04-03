d8d47f1ab62bfd5d18bbc68704a37001b347c467
derslik1-11 <sinifsoftito@gmail.com>
2024-04-03 13:41:17 +0300

    Refactor - public access modifier removed from interface

--- WatchWise/Repositories/Interfaces/IMediaRepository.cs
+++ WatchWise/Repositories/Interfaces/IMediaRepository.cs
@@ -5,22 +5,22 @@ namespace WatchWise.Repositories.Interfaces
 {
     public interface IMediaRepository
     {
-        public IQueryable<Media> GetAllMedia(bool includeMediaGenres = false
+        IQueryable<Media> GetAllMedia(bool includeMediaGenres = false
             , bool includeMediaStars = false
             , bool includeMediaDirectors = false
             , bool includeMediaRestrictions = false
             , bool includeUserFavorites = false);
-        public Media? GetMediaById(int id, bool includeMediaGenres = false
+        Media? GetMediaById(int id, bool includeMediaGenres = false
             , bool includeMediaStars = false
             , bool includeMediaDirectors = false
             , bool includeMediaRestrictions = false
             , bool includeUserFavorites = false);
-        public Media? GetMediaByName(string name, bool includeMediaGenres = false
+        Media? GetMediaByName(string name, bool includeMediaGenres = false
             , bool includeMediaStars = false
             , bool includeMediaDirectors = false
             , bool includeMediaRestrictions = false
             , bool includeUserFavorites = false);
-        public void AddMedia(Media media);
-        public void UpdateMedia(Media media);
+        void AddMedia(Media media);
+        void UpdateMedia(Media media);
     }
 }
