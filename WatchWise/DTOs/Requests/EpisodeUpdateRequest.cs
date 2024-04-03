599ec5555bd13df1381141c0903a9a4441fdb5eb
derslik1-11 <sinifsoftito@gmail.com>
2024-04-03 13:36:49 +0300

    Add - DTO for updating episode

---
+++ WatchWise/DTOs/Requests/EpisodeUpdateRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace WatchWise.DTOs.Requests
+{
+	public class EpisodeUpdateRequest
+	{
+        public string? Title { get; set; }
+
+        [StringLength(500)]
+        public string? Description { get; set; }
+    }
+}
+
