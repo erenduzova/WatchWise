e78a3a9d812abee5f0675dd10c7284f53a0cd948
derslik1-11 <sinifsoftito@gmail.com>
2024-04-03 15:36:44 +0300

    Add - MediaDirectors Repository and interface

---
+++ WatchWise/Repositories/Implementations/MediaDirectorRepository.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using WatchWise.Data;
+using WatchWise.Models;
+using WatchWise.Repositories.Interfaces;
+
+namespace WatchWise.Repositories.Implementations
+{
+	public class MediaDirectorRepository : IMediaDirectorRepository
+	{
+        private readonly WatchWiseContext _context;
+
+        public MediaDirectorRepository(WatchWiseContext context)
+        {
+            _context = context;
+        }
+
+        public IQueryable<MediaDirector> GetAllMediaDirectors()
+        {
+            IQueryable<MediaDirector> mediaDirectors = _context.MediaDirectors;
+            return mediaDirectors;
+        }
+
+        public MediaDirector? GetMediaDirectorByMediaId(int id)
+        {
+            IQueryable<MediaDirector> mediaDirectors = _context.MediaDirectors;
+            return mediaDirectors.FirstOrDefault(md => md.MediaId == id);
+        }
+
+
+        public MediaDirector? GetMediaDirectorByDirectorId(int id)
+        {
+            IQueryable<MediaDirector> mediaDirectors = _context.MediaDirectors;
+            return mediaDirectors.FirstOrDefault(md => md.DirectorId == id);
+        }
+
+        public void AddMediaDirector(MediaDirector mediaDirector)
+        {
+            _context.MediaDirectors.Add(mediaDirector);
+            _context.SaveChanges();
+        }
+
+        public void DeleteMediaDirector(MediaDirector mediaDirector)
+        {
+            _context.MediaDirectors.Remove(mediaDirector);
+            _context.SaveChanges();
+        }
+    }
+}
+
---
+++ WatchWise/Repositories/Interfaces/IMediaDirectorRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using WatchWise.Models;
+
+namespace WatchWise.Repositories.Interfaces
+{
+	public interface IMediaDirectorRepository
+	{
+        IQueryable<MediaDirector> GetAllMediaDirectors();
+        MediaDirector? GetMediaDirectorByMediaId(int id);
+        MediaDirector? GetMediaDirectorByDirectorId(int id);
+        void AddMediaDirector(MediaDirector mediaDirector);
+        void DeleteMediaDirector(MediaDirector mediaDirector);
+    }
+}
+
