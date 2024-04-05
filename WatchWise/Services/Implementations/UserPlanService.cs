339d38b56493df2633746026b49ae02b5617e8d0
derslik1-11 <sinifsoftito@gmail.com>
2024-04-05 12:15:48 +0300

    Add - UserPal Service and Interface

---
+++ WatchWise/Services/Implementations/UserPlanService.cs
@@ -0,0 +1,57 @@
+﻿using WatchWise.DTOs.Converters;
+using WatchWise.DTOs.Requests;
+using WatchWise.DTOs.Responses;
+using WatchWise.Models.CrossTables;
+using WatchWise.Repositories.Interfaces;
+using WatchWise.Services.Interfaces;
+
+namespace WatchWise.Services.Implementations
+{
+	public class UserPlanService : IUserPlanService
+	{
+        private readonly IUserPlanRepository _userPlanRepository;
+        private readonly UserPlanConverter _userPlanConverter;
+
+        public UserPlanService(IUserPlanRepository userPlanRepository, UserPlanConverter userPlanConverter)
+        {
+            _userPlanRepository = userPlanRepository;
+            _userPlanConverter = userPlanConverter;
+        }
+
+        public List<UserPlanResponse> GetAllUserPlanResponses()
+        {
+            IQueryable<UserPlan> userPlans = _userPlanRepository.GetAllUserPlans();
+            return _userPlanConverter.Convert(userPlans.ToList());
+        }
+
+        public UserPlanResponse? GetUserPlanResponsesById(long id)
+        {
+            UserPlan? foundUserPlan = _userPlanRepository.GetUserPlanById(id);
+            if (foundUserPlan != null)
+            {
+                return _userPlanConverter.Convert(foundUserPlan);
+            }
+            return null;
+        }
+
+        public List<UserPlanResponse> GetUserPlanResponsesByUserId(long userId)
+        {
+            IQueryable<UserPlan> userPlans = _userPlanRepository.GetUserPlansByUserId(userId);
+            return _userPlanConverter.Convert(userPlans.ToList());
+        }
+
+        public List<UserPlanResponse> GetUserPlanResponsesByPlanId(short planId)
+        {
+            IQueryable<UserPlan> userPlans = _userPlanRepository.GetUserPlansByPlanId(planId);
+            return _userPlanConverter.Convert(userPlans.ToList());
+        }
+
+        public void AddUserPlan(UserPlanRequest userPlanRequest)
+        {
+            var userPlan = _userPlanConverter.Convert(userPlanRequest);
+            _userPlanRepository.AddUserPlan(userPlan);
+        }
+
+    }
+}
+
---
+++ WatchWise/Services/Interfaces/IUserPlanService.cs
@@ -0,0 +1,15 @@
+﻿using WatchWise.DTOs.Requests;
+using WatchWise.DTOs.Responses;
+
+namespace WatchWise.Services.Interfaces
+{
+	public interface IUserPlanService
+	{
+        List<UserPlanResponse> GetAllUserPlanResponses();
+        UserPlanResponse? GetUserPlanResponsesById(long id);
+        List<UserPlanResponse> GetUserPlanResponsesByUserId(long userId);
+        List<UserPlanResponse> GetUserPlanResponsesByPlanId(short planId);
+        void AddUserPlan(UserPlanRequest userPlanRequest);
+    }
+}
+
