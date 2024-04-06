0205128e7eb4131969b8f844f8601c1f4fbe972e
erenduzova <erenduzova@gmail.com>
2024-04-07 01:40:24 +0300

    Add - Default genres, plans, restrictions added.

--- WatchWise/Data/DbInitializer.cs
+++ WatchWise/Data/DbInitializer.cs
@@ -31,6 +31,18 @@ namespace WatchWise.Data
                 CreateUserManagerUser();
                 CreateRestrictionManagerUser();
             }
+            if (!_watchWiseContext.Genres.Any())
+            {
+                CreateGenres();
+            }
+            if (!_watchWiseContext.Plans.Any())
+            {
+                CreatePlans();
+            }
+            if (!_watchWiseContext.Restrictions.Any())
+            {
+                CreateRestrictions();
+            }
         }
 
         public void CreateRoles()
@@ -62,6 +74,7 @@ namespace WatchWise.Data
             _userManager.CreateAsync(adminUser, adminPassword).Wait();
             _userManager.AddToRoleAsync(adminUser, "Admin").Wait();
         }
+
         public void CreateContentManagerUser()
         {
             WatchWiseUser contentManagerUser = new()
@@ -108,6 +121,35 @@ namespace WatchWise.Data
             string restrictionManagerPassword = "Restriction123!";
             _userManager.CreateAsync(restrictionManagerUser, restrictionManagerPassword).Wait();
             _userManager.AddToRoleAsync(restrictionManagerUser, "RestrictionManager").Wait();
+        }
+
+        public void CreateGenres()
+        {
+            string[] genreNames = {"Action", "Adventure", "Animation", "Comedy", "Crime", "Documentary", "Drama", "Fantasy", "Horror", "Mystery", "Romance", "Science Fiction", "Thriller"};
+            foreach (string name in genreNames)
+            {
+                Genre genre = new() { Name = name };
+                _watchWiseContext.Genres.Add(genre);
+            }
+            _watchWiseContext.SaveChanges();
+        }
+
+        public void CreatePlans()
+        {
+            _watchWiseContext.Plans.Add(new Plan() { Name = "Basic", Price = 119.99f, MaxResolution = "720p" });
+            _watchWiseContext.Plans.Add(new Plan() { Name = "Standard", Price = 176.99f, MaxResolution = "1080p" });
+            _watchWiseContext.Plans.Add(new Plan() { Name = "Premium", Price = 119.99f, MaxResolution = "4K" });
+            _watchWiseContext.SaveChanges();
+        }
+
+        public void CreateRestrictions()
+        {
+            _watchWiseContext.Restrictions.Add(new Restriction() { Id = 0, Name = "General Audience" });
+            _watchWiseContext.Restrictions.Add(new Restriction() { Id = 7, Name = "7 and older" });
+            _watchWiseContext.Restrictions.Add(new Restriction() { Id = 13, Name = "13 and older" });
+            _watchWiseContext.Restrictions.Add(new Restriction() { Id = 18, Name = "18 and older" });
+
+            _watchWiseContext.SaveChanges();
         }
     }
 }
