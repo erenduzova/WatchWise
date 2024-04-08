2e803fe0f2ab6a7051267e9e1382282f41503c49
erenduzova <erenduzova@gmail.com>
2024-04-08 14:33:08 +0300

    Add - Role Repository and interface added

---
+++ WatchWise/Repositories/Implementations/RoleRepository.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Identity;
+using WatchWise.Models;
+using WatchWise.Repositories.Interfaces;
+
+namespace WatchWise.Repositories.Implementations
+{
+    public class RoleRepository : IRoleRepository
+    {
+        private readonly RoleManager<WatchWiseRole> _roleManager;
+        private readonly UserManager<WatchWiseUser> _userManager;
+
+        public RoleRepository(RoleManager<WatchWiseRole> roleManager, UserManager<WatchWiseUser> userManager)
+        {
+            _roleManager = roleManager;
+            _userManager = userManager;
+        }
+
+        public IQueryable<WatchWiseRole> GetAllRoles()
+        {
+            return _roleManager.Roles;
+        }
+
+        public IList<string> GetUserRoles(WatchWiseUser user)
+        {
+            return _userManager.GetRolesAsync(user).Result;
+        }
+
+        public IdentityResult AddToRole(WatchWiseUser user, string roleName)
+        {
+            return _userManager.AddToRoleAsync(user, roleName).Result;
+        }
+
+        public IdentityResult RemoveFromRole(WatchWiseUser user, string roleName)
+        {
+            return _userManager.RemoveFromRoleAsync(user, roleName).Result;
+        }
+
+    }
+}
---
+++ WatchWise/Repositories/Interfaces/IRoleRepository.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+using WatchWise.Models;
+
+namespace WatchWise.Repositories.Interfaces
+{
+    public interface IRoleRepository
+    {
+        IQueryable<WatchWiseRole> GetAllRoles();
+        IList<string> GetUserRoles(WatchWiseUser user);
+        IdentityResult AddToRole(WatchWiseUser user, string roleName);
+        IdentityResult RemoveFromRole(WatchWiseUser user, string roleName);
+    }
+}
